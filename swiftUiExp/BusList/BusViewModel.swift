6d601ac49b4387bf123a0a713c54b4f0f44fa341
Sagar <sagarcoc2000@gmail.com>
2024-06-13 11:11:26 +0530

    merged

--- swiftUiExp/BusList/BusViewModel.swift
+++ swiftUiExp/BusList/BusViewModel.swift
@@ -20,7 +20,7 @@ class BusServiceViewModel: ObservableObject {
     @Published var sortedBusServices: [BusService] = []
     
     init() {
-            availableBusService(source: 3, destination: 5, date: "2024-06-11")
+            availableBusService(source: 3, destination: 5, date: "2024-06-13")
     }
 
     func availableBusService(source: Int, destination: Int, date: String) {
